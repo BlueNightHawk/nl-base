f98e3154d0f9bcd32797fa8c83fd2d8d7c653b29
BlueNightHawk <46296596+BlueNightHawk@users.noreply.github.com>
2023-02-03 13:13:25 +0530

    Fix linux build

--- cl_dll/reGS_enginehook.h
+++ cl_dll/reGS_enginehook.h
@@ -41,10 +41,10 @@ extern Utils utils;
 #else
 #define Hook(future_name, hook)                                                                                                               \
 	{                                                                                                                                         \
-		ORIG_##future_name = decltype(ORIG_##future_name) PL_GetProcAddress(utils.GetHandle(), #future_name);                                  \
+		ORIG_##future_name = decltype(ORIG_##future_name)(PL_GetProcAddress(utils.GetHandle(), #future_name));                                  \
 		if (ORIG_##future_name)                                                                                                             \
 		{                                                                                                                                     \
-			gEngfuncs.Con_DPrintf("[%s] Found " #future_name " at %p (using the %s pattern).\n", HWEXT, ORIG_##future_name, pattern->name()); \
+			gEngfuncs.Con_DPrintf("[%s] Found " #future_name " at %p.\n", HWEXT, ORIG_##future_name); \
 			void* p##future_name = (void*)ORIG_##future_name;                                                                                 \
 			hook.Install(p##future_name, (void*)future_name);                                                                                 \
 		}                                                                                                                                     \
