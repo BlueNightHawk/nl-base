526fc98525a5accdc63b3f6c139c39aaf1da337b
BlueNightHawk <46296596+BlueNightHawk@users.noreply.github.com>
2023-02-03 14:10:10 +0530

    Hook buildnum

--- cl_dll/reGS_enginehook.cpp
+++ cl_dll/reGS_enginehook.cpp
@@ -5,6 +5,14 @@
 
 Utils utils = Utils::Utils(NULL, NULL, NULL);
 
+typedef int (*_build_number)();
+_build_number ORIG_build_number = nullptr;
+subhook::Hook build_numberHook;
+
+int build_number();
+void HookBuildNum();
+void UnookBuildNum();
+
 bool HWHook()
 {
 	void* handle;
@@ -19,6 +27,7 @@ bool HWHook()
 	VGuiWrap2_Hook();
 	R_Hook();
 	GLDraw_Hook();
+	HookBuildNum();
 
 	return true;
 }
@@ -28,4 +37,54 @@ void ShutdownHooks()
 	VGuiWrap2_UnHook();
 	R_UnHook();
 	GLDraw_UnHook();
+	UnookBuildNum();
+}
+
+// reverse-engineered by oxiKKK
+// https://github.com/oxiKKK/goldsrc-reversed-build_number-function/blob/main/build_num.h
+
+#define RELEASE_DAY 34995 // Possibly Thursday, October 24, 1996
+
+const char* date = __DATE__;
+
+char* mon[12] =
+	{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+char mond[12] =
+	{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int build_number()
+{
+	int m = 0;
+	int d = 0;
+	int y = 0;
+	int build = 0;
+
+	for (m = 0; m < 11; m++)
+	{
+		if (strnicmp(&date[0], mon[m], 3) == 0)
+			break;
+		d += mond[m];
+	}
+
+	d += atoi(&date[4]) - 1;
+
+	y = atoi(&date[7]) - 1900;
+
+	build = d + (int)((y - 1) * 365.25);
+
+	if (((y % 4) == 0) && m > 1)
+		build += 1;
+
+	build -= RELEASE_DAY;
+	return build;
+}
+
+void HookBuildNum()
+{
+	Hook(build_number, build_numberHook);
+}
+
+void UnookBuildNum()
+{
+	build_numberHook.Remove();
 }
