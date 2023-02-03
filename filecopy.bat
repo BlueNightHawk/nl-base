913ddb6b578a25e01813a8ade6814124ae191a56
BlueNightHawk <46296596+BlueNightHawk@users.noreply.github.com>
2023-02-03 14:31:39 +0530

    Update filecopy.bat

--- filecopy.bat
+++ filecopy.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Set this path to your mod directory. You can use environment variables to avoid hardcoding the path if your team members have different install locations.
-set mod_directory=E:\SteamLibrary\steamapps\common\Half-Life/halflife_updated
+set mod_directory=E:\SteamLibrary\steamapps\common\Half-Life\nl_base
 
 rem Input paths may end with a backslash which will be interpreted as an escape character when passed into robocopy, so this needs to be escaped.
 set source=%~1
